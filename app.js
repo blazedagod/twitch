c5a2c00d29d784a6d21ef7ba018c6bf7e85701b4
blazedagod <40705953+blazedagod@users.noreply.github.com>
2018-12-18 23:08:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Following from "./components/following";
+import TopStreams from "./components/topstreams";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => (
   <Router>
     <div>
       <Route path="/" exact component={Following} />
-      <Route
-        path="/topstreams"
-        exact
-        component={() => <div>Top Streams</div>}
-      />
+      <Route path="/topstreams" exact component={TopStreams} />
       <Route path="/topgames" exact component={() => <div>Top Games</div>} />
     </div>
   </Router>
