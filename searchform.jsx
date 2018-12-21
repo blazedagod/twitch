057aeef882e7efbe22db9dff758be1e5ac630100
blazedagod <40705953+blazedagod@users.noreply.github.com>
2018-12-21 03:29:46 -0500

    Add files via upload

--- searchform.jsx
+++ searchform.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Input } from "antd";
 import "./app.css";
+import { myContext } from "./twitchcontext";
 
-const SearchForm = props => (
-  <div className="searchForm">
-    <h2>See whose online</h2>
+const SearchForm = () => {
+  const { onChange, onlineStreamers, searchtext, userinput } = useContext(
+    myContext
+  );
+  return (
+    <div className="searchForm">
+      <h2>See whose online</h2>
 
-    <form onSubmit={props.onSubmit}>
-      <Input
-        onChange={props.onChange}
-        type="text"
-        placeholder={props.searchtext}
-        value={props.userinput}
-      />
-      <br />
-      <Button type="primary">search</Button>
-    </form>
-  </div>
-);
+      <form onSubmit={onlineStreamers}>
+        <Input
+          onChange={onChange}
+          type="text"
+          placeholder={searchtext}
+          value={userinput}
+        />
+        <br />
+        <Button type="primary">search</Button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchForm;
